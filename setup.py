6f9d61d560cf1468718a160959cb191f5cd88b41
andela-lolo <loice.andia@andela.com>
2016-10-04 17:15:08 +0300

    [Chore #131631749] Intergrate tox, travis and coverage

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='bucket-list-application-api',
+    description='Checkpoint 2',
+    author='Loice Andia',
+    author_email='loice.andia@andela.com',
+    url='https://github.com/andela-lolo/bucket-list-application-api',
+    version='1.0.0'
+)

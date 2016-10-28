6fe9e46a6eb4c9bae21ebf1924ebf1e34b6d4f18
andela-lolo <loice.andia@andela.com>
2016-10-28 21:35:14 +0300

    [Fix #131756355] Fix mysql intergration on travis

--- app/test_config.py
+++ app/test_config.py
@@ -1,9 +1,11 @@
 from flask_testing import TestCase
 from run import app
+from sqlalchemy import create_engine
 
 
 class GlobalTestCase(TestCase):
     def create_app(self):
+        create_engine('mysql://root:root@localhost/bucketlist_test')
         app.config['SQLALCHEMY_DATABASE_URI'] = \
             "mysql://root:root@localhost/bucketlist_test"
         app.config['TESTING'] = True

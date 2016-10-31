88c9fac6d77980b680b912495aba8bfccf716ddb
andela-lolo <loice.andia@andela.com>
2016-10-31 12:55:59 +0300

    [Fix #131756355] Remove password configuration on travis

--- app/test_config.py
+++ app/test_config.py
@@ -5,7 +5,7 @@ from run import app
 class GlobalTestCase(TestCase):
     def create_app(self):
         app.config['SQLALCHEMY_DATABASE_URI'] = \
-            "mysql://root:root@localhost/bucketlist_test"
+            "mysql://root@localhost/bucketlist_test"
         app.config['TESTING'] = True
         app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = False
         return app

161ea17feff8172f3af445a3cee490a3ecb777e9
andela-lolo <loice.andia@andela.com>
2016-10-31 23:34:31 +0300

    [Feature #133437737] Implement login basic post

--- app/auth/login.py
+++ app/auth/login.py
@@ -1,14 +1,39 @@
-from flask_restful import Resource
+import json
+from flask import jsonify, request
+from flask_restful import abort, Resource
 
 
 class Index(Resource):
+
     def get(self):
-        pass
+        return jsonify({"message": "Welcome to the BucketList API."
+                        " Register a new user by sending a"
+                        " POST request to /auth/register. "
+                        "Login by sending a POST request to"
+                        " /auth/login to get started."})
 
 
 class Login(Resource):
     def get(self):
-        pass
+        return jsonify({"message": "To login,"
+                        "send a POST request to /auth/login."})
 
     def post(self):
-        pass
+        data = json.loads(request.get_data(as_text=True))
+        if not data:
+            abort(400,
+                  message="No params passed. Kindly fill you username, email and password")
+        username = data['username']
+        password = data['password']
+
+        if not username or not password:
+            abort(400,
+                  message="Kindly fill in the missing details")
+
+        if len(password) < 4:
+            abort(400,
+                  message="Password should be 4 or more characters")
+
+        user = Users.query.filter_by(username=username).first()
+        if user is None:
+            abort(400, message="User does not exist")
